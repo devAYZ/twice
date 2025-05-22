ae382d85925a5bcd13818ac60233cf4e34c6881e
devayz <fatokimiemman@gmail.com>
2025-05-22 08:46:10 +0100

    Initial Commit

---
+++ twice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  twice
+//
+//  Created by Ayokunle Fatokimi on 22/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ twice/twiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  twiceApp.swift
+//  twice
+//
+//  Created by Ayokunle Fatokimi on 22/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct twiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
